4c614fb4c05333520905b1059e02df508f13e251
Aaron <114060853+hecabre@users.noreply.github.com>
2024-05-15 22:43:47 -0600

    command for backend

--- server/app.js
+++ server/app.js
@@ -4,11 +4,14 @@ import uamRouter from "./routes/uam.routes.js";
 import ipnRouter from "./routes/ipn.routes.js";
 import universityRouter from "./routes/university.routes.js";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.use(cors({ origin: "https://womantech.vercel.app", credentials: true }));
 
 app.use("/api/unam", unamRouter);
 app.use("/api", universityRouter);
