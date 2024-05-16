89624bf182d19db22b3ef5994ed7fe7c9ac3ccfc
Aaron <114060853+hecabre@users.noreply.github.com>
2024-05-15 21:35:30 -0600

    env added

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ export async function connectDb() {
   try {
     const connection = await mysql.createConnection({
       host: process.env.HOST,
-      port: process.env.PORT,
+      port: process.env.PORT_DB,
       user: process.env.USER,
       password: process.env.PASSWORD,
       database: process.env.DATABASE,
--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ import dotenv from "dotenv";
 import { connectDb } from "./db.js";
 
 dotenv.config();
+
 connectDb();
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 console.log("Server on port 3000");
